4278e1ad226e47d7ccf194162676ff7ecdd4970b
Florencia Rodil <rodil.florencia@gmail.com>
2023-06-16 21:12:55 -0300

    Agregando script, nombres y links al repo

---
+++ generar.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#ver como guarda la ruta de las imgs dentro de la carpeta
+wget -O varPerson.jpg -P /home/alumno/EdP16.06/imgs_web/ https://source.unsplash.com/random/900%C3%97700/?person
+
