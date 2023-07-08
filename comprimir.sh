caf923b3c890ca1ea7f389d10f2a10d182b5ce53
Florencia Rodil <rodil.florencia@gmail.com>
2023-07-08 20:23:45 -0300

    Se cambió el nombre del directorio temp a procesados

--- comprimir.sh
+++ comprimir.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#Script Comprimir
 
 RUTA_C=./dataset/comprimidos
-RUTA_D=./dataset/descomprimidos
+RUTA_P=./dataset/procesados
 
 # Verifica si existe el directorio comprimidos en el directorio dataset, si no existe lo crea.
 if [ ! -d $RUTA_C ]; then
@@ -9,23 +10,23 @@ if [ ! -d $RUTA_C ]; then
 fi
 
 # Mueve los archivos img_names.txt y img_names_val.txt al directorio temp.
-mv "$RUTA_D"/img_names.txt "$RUTA_D"/temp
-mv "$RUTA_D"/img_names_val.txt "$RUTA_D"/temp
+#mv "$RUTA_D"/img_names.txt "$RUTA_D"/temp
+#mv "$RUTA_D"/img_names_val.txt "$RUTA_D"/temp
 
 # Genera un archivo con los nombres y el total de nombres finalizados en "a".
 IFS=$'\n'
 CANT=0
-for LINE in $(cat "$RUTA_D"/temp/img_names_val.txt); do
+for LINE in $(cat "$RUTA_P"/img_names_val.txt); do
 	NAME=$(basename "$LINE" .jpg)
 	if [[ ${NAME: -1} == "a" ]]; then
 		CANT=$((CANT + 1))
-		echo $LINE >> "$RUTA_D"/temp/img_female.txt
+		echo $LINE >> "$RUTA_P"/img_female.txt
 	fi
 done	
-echo "La cantidad total de personas cuyo nombre termina en "a" es: $CANT" >> "$RUTA_D"/temp/img_female.txt
+echo "La cantidad total de personas cuyo nombre termina en "a" es: $CANT" >> "$RUTA_P"/img_female.txt
 
-# Comprime todos los archivos del directorio temp.
+# Comprime todos los archivos del directorio procesados.
 echo "Comprimiendo..."
 sleep 2
 NOMBRE=$(date +"%d-%m-%y_%H:%M")
-tar -cvf $RUTA_C/$NOMBRE.tar ./dataset/descomprimidos/temp/
+tar -cvf $RUTA_C/$NOMBRE.tar $RUTA_P
